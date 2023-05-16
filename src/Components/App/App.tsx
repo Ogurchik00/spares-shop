4ef3a4e5f27937df021cc5ac1643d219bd7fc596
Ogurchik-007 <nik.skripka.000@gmail.com>
2023-05-16 08:45:15 +0300

    creating a basic project structure

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
 
-function App() {  
+import './App.scss';
+
+const App = () => {  
 
   return (
     <div className="App">
-      
+      ff
     </div>
   );
 }
